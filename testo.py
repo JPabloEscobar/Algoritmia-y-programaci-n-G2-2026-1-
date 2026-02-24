96314cdb314d685cd9254e0e06aad640ba5aeba5
JPablo Escobar <pablo.escobar@utp.edu.co>
2026-02-24 18:03:48 -0500

    Primera Iteracion

---
+++ testo.py
@@ -0,0 +1,30 @@
+print("Bienvenidos al Hotel-PICHINCHA")
+# piso = int(input("Ingrese un piso: "))
+# boton = input("CerrarPuerta: ")
+
+
+# if piso == 0:
+#     print("Estas en el Looby")
+#     if boton == "Cerrar":
+#         print("Cerrar puertas")
+#     elif boton == "NoCerrar":
+#         print("Esperar 25 segundos")
+# if piso == 1:
+#     print("Estas en el primer piso")
+#     if boton == "Cerrar":
+#         print("Cerrar puertas")
+#     elif boton == "NoCerrar":
+#         print("Esperar 25 segundos")
+# elif piso == 2:
+#     print("Estas en el segundo piso")
+#     if boton == "Cerrar":
+#         print("Cerrar puertas")
+#     elif boton == "NoCerrar":
+#         print("Esperar 25 segundos")
+# elif piso == 3:
+#     print("Estas en el tercer piso")
+#     if boton == "Cerrar":
+#         print("Cerrar puertas")
+#     elif boton == "NoCerrar":
+#         print("Esperar 25 segundos")
+
