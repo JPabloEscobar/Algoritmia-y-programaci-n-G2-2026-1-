bf0059b63cad4fcd098b1e4820e552343bc1a685
JPablo Escobar <pablo.escobar@utp.edu.co>
2026-03-04 10:23:02 -0500

    EjerciciosListasCondi

---
+++ Listas/EjerciciosListas.py
@@ -0,0 +1,28 @@
+#TODO: Implemente los ejercicios para de listas y condicionales.
+"""
+ 1.Una empresa de transporte tiene una lista con los nombres de sus conductores.
+   Se pide realizar un programa que permita ingresar el nombre de un conductor y 
+   verificar si se encuentra en la lista.
+
+ 2. Una empresa de energía quiere llevar un registro de sus clientes y su consumo
+    de energía. Se pide realizar un programa que permita ingresar el nombre del 
+    cliente y su consumo en kWh, y luego calcular el costo total a pagar considerando
+    que el precio por kWh es de $0.15. 
+
+ 3. Una tienda tiene en su base de datos los siguientes productos:
+     ______________________
+     | Producto  | Precio |
+     |-----------|--------|
+     | Manzana   | $2.5   |
+     | Banana    | $1.8   |
+     | Naranja   | $3.0   |
+     | Pera      | $2.0   |
+     | Monster   | $5.0   |
+     | Agua      | $1.0   |
+     | Coca-Cola | $3.5   |
+     |--------------------|
+    
+     La propretaria de la tienda le necesita un sistema que le ayude a gestionar
+     su inventario. El sistema debe permitirle: verificar si un producto se encuentra en el inventario,
+     agregar nuevos productos con su precio, eliminar productos y actualizar el precio de un producto existente.
+"""
--- Listas/listas.py
+++ Listas/listas.py
@@ -1,3 +1,4 @@
+print("Liastas\n")
 Lista = [1, 2.3, "Hola", True]
 
 agregar = Lista.append("Mundo") #agregar un elemento al final de la lista
@@ -5,10 +6,23 @@ agregar2 = Lista.insert(1, "Python") #agregar un elemento en la posición 1 de l
 quitar1 = Lista.remove(2.3) #quita la "variable" 2.3 de la lista
 quitar2 = Lista.pop(0) #quita el elemento en la posición 0 de la lista si no se quita el último elemento de la lista
 
-print(Lista)
+listaVacia = []
+#print(listaVacia.append("2",43,"funciona"))
+print(listaVacia.append("2"))
+print(listaVacia.append(43))
+print(listaVacia.append("funciona"))
 
+print(Lista,"\n")
+print(listaVacia)
+
+
+print("\nDiccionarios\n")
 Diccionario = {"numero": 1,
                 2: "dos",
                 2.3: True}
 
-print(Diccionario["numero"]) #imprime el valor de la clave (key) "numero"
+#print(Diccionario["numero"]) #imprime el valor de la clave (key) "numero"
+Diccionario["numero"] = 10 #cambia el valor de la clave "numero" a 10
+Diccionario["nuevo"] = "nuevo valor" #agrega una nueva clave "nuevo" con el valor "nuevo valor"
+print(Diccionario.pop("test", "No existe la clave")) #quita la clave 2.3 y devuelve su valor, si no existe devuelve "No existe la clave"
+print(Diccionario)
